a5da6b0b4a59141256e9161e6f0465695fd54428
PugazhendhiK03 <pugazhendhikrishna03@gmail.com>
2025-02-28 13:13:17 +0530

    project card design changed

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,6 @@
 const projects = [
-    { title: "Courier Management System", tech: "PHP, CSS, JS, MYSQL", description: "Built a web-based system for tracking deliveries, managing customer data, and optimizing courier operations", image: "img/CMS.png"},
-    { title: "Enhance the Surveillance System Using Face Recognition and Object Detection", tech: "Python, Flask, HTML, CSS, JS", description: "Developed a system for identifying individuals and detecting objects in surveillance footage with a searchable timestamp feature.", image: "img/Project2.png" }
+    { title: "Courier Management System", tech: "PHP", description: "Built a web-based system for tracking deliveries, managing customer data, and optimizing courier operations", image: "img/CMS.png"},
+    { title: "Enhance the Surveillance System Using Face Recognition and Object Detection", tech: "Python", description: "Developed a system for identifying individuals and detecting objects in surveillance footage with a searchable timestamp feature.", image: "img/Project2.png" }
 ];
 
 let currentPage = 1;
@@ -18,10 +18,11 @@ function displayProjects(page) {
                 <span class="project-links">
                 <a href="#"><i class="fa-solid fa-globe"></i></a>
                 <a href="#"><i class="fa-brands fa-github"></i></a>
+                <span class="tech-label">${project.tech}</span>
                 </span>
                 <img src="${project.image}" alt="${project.title}">
             </div>
-            <span class="tech-label">${project.tech}</span>
+            
             <span>${project.title}</span>
             <span>${project.description}</span>
         </div>
