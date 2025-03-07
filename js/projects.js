684250e41b3480a12cc4a852b43c88a10e74e40c
PugazhendhiK03 <pugazhendhikrishna03@gmail.com>
2025-03-07 13:15:17 +0530

    project dynamic function added

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,20 @@
 const projects = [
-    { title: "Courier Management System", tech: "PHP", description: "Built a web-based system for tracking deliveries, managing customer data, and optimizing courier operations", image: "img/CMS.png"},
-    { title: "Enhance the Surveillance System Using Face Recognition and Object Detection", tech: "Python", description: "Developed a system for identifying individuals and detecting objects in surveillance footage with a searchable timestamp feature.", image: "img/Project2.png" }
+    {
+        title: "Courier Management System",
+        tech: "PHP",
+        description: "Built a web-based system for tracking deliveries, managing customer data, and optimizing courier operations",
+        image: "img/CMS.png",
+        deployedLink: "#", 
+        githubLink: "https://github.com/PugazhendhiK03/mini-project"
+    },
+    {
+        title: "Enhance the Surveillance System Using Face Recognition and Object Detection",
+        tech: "Python",
+        description: "Developed a system for identifying individuals and detecting objects in surveillance footage with a searchable timestamp feature.",
+        image: "img/Project2.png",
+        deployedLink: "#", 
+        githubLink: "https://github.com/PugazhendhiK03/final-year-project"
+    }
 ];
 
 let currentPage = 1;
@@ -10,24 +24,23 @@ function displayProjects(page) {
     const start = (page - 1) * projectsPerPage;
     const end = start + projectsPerPage;
     const displayedProjects = projects.slice(start, end);
-    
+
     const projectsGrid = document.getElementById("projectsGrid");
     projectsGrid.innerHTML = displayedProjects.map(project => `
         <div class="project-card">
             <div class="project-demo">
                 <span class="project-links">
-                <a href="#"><i class="fa-solid fa-globe"></i></a>
-                <a href="#"><i class="fa-brands fa-github"></i></a>
-                <span class="tech-label">${project.tech}</span>
+                    <a href="${project.deployedLink}" target="_blank"><i class="fa-solid fa-globe"></i></a>
+                    <a href="${project.githubLink}" target="_blank"><i class="fa-brands fa-github"></i></a>
+                    <span class="tech-label">${project.tech}</span>
                 </span>
                 <img src="${project.image}" alt="${project.title}">
             </div>
-            
             <span>${project.title}</span>
             <span>${project.description}</span>
         </div>
     `).join("");
-    
+
     document.querySelectorAll(".dot").forEach((dot, index) => {
         dot.classList.toggle("active", index + 1 === page);
     });
